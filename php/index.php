fb0db58b87576b221179bae96aec0dedf66e5356
Guillaume <gui.grc@yahoo.com>
2020-11-16 18:32:49 +0100

    Module Connexion. Html et css de inscription et index Ok

---
+++ php/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Vikings, road to Valhalla MMORPG</title>
+    <link rel="stylesheet" href="../css/index.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <ul class="nav justify-content-center nav-head">
+                <li class="nav-item"><a class="nav-link" href="inscription.php">| Rejoinde l'Aventure</a></li>
+                <li class="nav-item"><a class="nav-link" href="connexion.php">| Connexion avec le Valhalla</a></li>
+                <li class="nav-item"><a class="nav-link" href="profil.php">| Mon Profil</a></li>
+                <li class="nav-item"><a class="nav-link" href="admin.php">| Le grand Hall (réservé aux dieux)</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section id="titre" class="container-fluid">
+            <h1>Road To Valhalla</h1>
+        </section>
+        <section class="container-fluid boutons">
+            <section class="container-fluid bouton-1">
+                <a class="btn-grad" href="inscription.php">Rejoindre l'Aventure <i class="fas fa-crow"></i><br>(Inscription)</a>
+            </section>
+            <section class="container-fluid bouton-2">
+                <a class="btn-grad-2" href="connexion.php">Connexion avec le Valhalla <i class="fas fa-dungeon"></i><br>(Connexion au compte)</a>
+            </section>
+        </section>
+    </main>
+    <footer>
+        <p>Vikings, Road to Valhalla 1.0.1</p>
+    </footer>
+</body>
+</html>
---
+++ php/inscription.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Inscription</title>
+    <link rel="stylesheet" href="../css/inscription.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <ul class="nav justify-content-center nav-head">
+                <li class="nav-item"><a class="nav-link" href="index.php">| Accueil</a></li>
+                <li class="nav-item"><a class="nav-link" href="connexion.php">| Connexion avec le Valhalla</a></li>
+                <li class="nav-item"><a class="nav-link" href="profil.php">| Mon Profil</a></li>
+                <li class="nav-item"><a class="nav-link" href="admin.php">| Le grand Hall (réservé aux dieux)</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section class="container-fluid">
+            <form method="post" action="inscription.php"> 
+                <section class="form-group">
+                    <label for="login">Login</label>
+                    <input type="text" class="form-control" id="login" placeholder="Votre Login" required> 
+                </section>
+                <section class="form-group">
+                    <label for="nom">Nom</label>
+                    <input type="text" class="form-control" id="nom" placeholder="Votre Nom" required>
+                </section>
+                <section class="form-group">
+                    <label for="login">Prénom</label>
+                    <input type="text" class="form-control" id="login" placeholder="Votre Prénom" required>
+                </section>
+                <section class="form-group">
+                    <label for="password">Mot de Passe</label>
+                    <input type="password" class="form-control" id="password" placeholder="Votre Mot de Passe" required>
+                </section>
+                <section class="form-group">
+                    <label for="password">Confirmez votre Mot de Passe</label>
+                    <input type="password" class="form-control" id="password" placeholder="Confirmez votre Mot de Passe" required>
+                </section>
+                <button type="submit" class="btn btn-dark">Rejoignez la bataille !</button>
+            </form>
+        </section>
+    </main>
+    <footer>
+        <p>Vikings, Road to Valhalla 1.0.1</p>
+    </footer> 
+</body>
+</html>
